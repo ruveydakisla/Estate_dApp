304eb2d21b6c63f385564815c90e5f13dccce27c
ruveydakisla <ruveydakisla34@gmail.com>
2023-12-20 12:56:04 +0300

    contract functions added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
 import { EthProvider } from "./contexts/EthContext";
 
 import ContractBtns from "./components/Demo/ContractBtns";
+import Navbar from "./pages/Navbar";
 
 function App() {
   return (
     <EthProvider>
       <div id="App">
         <div className="container">
-          <ContractBtns/>
+          <Navbar/>
         </div>
       </div>
     </EthProvider>
