b1d009038f58017a555b736636eaae8f7a18cea7
Omer Faruk KUMRAL <the.kumral44@gmail.com>
2023-12-20 20:50:21 +0300

    fix

--- client/src/components/Pages/PropertyAdd.jsx
+++ client/src/components/Pages/PropertyAdd.jsx
@@ -15,88 +15,89 @@ export default function PropertyAdd() {
 
   const addProperty = async () => {
     await contract.methods
-      .addProperty(propertyName, propertyAddress, propertyImage,propertyType)
+      .addProperty(propertyName, propertyAddress, propertyImage, propertyType)
       .send({ from: accounts[0] });
   };
 
-  const getEstates=async()=>{
-    const datas=await contract.methods.getProperties().call({from:accounts[0]});
+  const getEstates = async () => {
+    const datas = await contract.methods
+      .getProperties()
+      .call({ from: accounts[0] });
     convertObjects(datas);
     console.log(estates);
-    
-  }
+  };
 
-  const estates=[];
-  const convertObjects=(array)=>{
-
-    for(var i=0;i<array.length;i++){
-      const estate={
-        propertyName:array[i][0],
-        propertyOwner:array[i][1],
-        propertyAddress:array[i][2],
-        propertyImage:array[i][3],
-        propertyType:array[i][4]
-        
+  const estates = [];
+  const convertObjects = (array) => {
+    for (var i = 0; i < array.length; i++) {
+      const estate = {
+        propertyName: array[i][0],
+        propertyOwner: array[i][1],
+        propertyAddress: array[i][2],
+        propertyImage: array[i][3],
+        propertyType: array[i][4],
       };
       estates.push(estate);
     }
-   
-  }
-
+  };
 
   return (
     <div className="container">
       <div className="PropertyAdd-container">
         <h1>Property Add</h1>
-        
-          <div className="input-group">
-            <label htmlFor="propertyName">Property Name:</label>
-            <input
-              type="text"
-              id="propertyName"
-              name="propertyName"
-              value={propertyName}
-              onChange={(e) => setPropertyName(e.target.value)}
-              required
-            />
-          </div>
-          <div className="input-group">
-            <label htmlFor="propertyAddress">Property Address:</label>
-            <input
-              type="text"
-              id="propertyAddress"
-              name="propertyAddress"
-              value={propertyAddress}
-              onChange={(e) => setPropertyAddress(e.target.value)}
-              required
-            />
-          </div>
-          <div className="input-group">
-            <label htmlFor="propertyImage">Property Image:</label>
-            <input
-              type="text"
-              id="propertyImage"
-              name="propertyImage"
-              value={propertyImage}
-              onChange={(e) => setPropertyImage(e.target.value)}
-              required
-            />
-          </div>
-          <div className="input-group" >
-            <label htmlFor="propertyType">Property Type:</label>
-            <select onChange={(e)=>setPropertyType(e)} value={propertyType} id="propertyType" name="propertyType" required>
-              <option value="apartment">Apartment</option>
-              <option value="house">House</option>
-              <option value="office">Office</option>
-            </select>
-          </div>
-          <button onClick={addProperty}>Add Property</button>
-          <br />
-          <br />
-          <button onClick={getEstates}>get estates</button>
-        <div>
-          
+
+        <div className="input-group">
+          <label htmlFor="propertyName">Property Name:</label>
+          <input
+            type="text"
+            id="propertyName"
+            name="propertyName"
+            value={propertyName}
+            onChange={(e) => setPropertyName(e.target.value)}
+            required
+          />
         </div>
+        <div className="input-group">
+          <label htmlFor="propertyAddress">Property Address:</label>
+          <input
+            type="text"
+            id="propertyAddress"
+            name="propertyAddress"
+            value={propertyAddress}
+            onChange={(e) => setPropertyAddress(e.target.value)}
+            required
+          />
+        </div>
+        <div className="input-group">
+          <label htmlFor="propertyImage">Property Image:</label>
+          <input
+            type="text"
+            id="propertyImage"
+            name="propertyImage"
+            value={propertyImage}
+            onChange={(e) => setPropertyImage(e.target.value)}
+            required
+          />
+        </div>
+        <div className="input-group">
+          <label htmlFor="propertyType">Property Type:</label>
+          <select
+            onChange={(e) => setPropertyType(e)}
+            value={propertyType}
+            id="propertyType"
+            name="propertyType"
+            required
+          >
+            <option value="apartment">Apartment</option>
+            <option value="house">House</option>
+            <option value="office">Office</option>
+          </select>
+        </div>
+        <button onClick={addProperty}>Add Property</button>
+        <br />
+        <br />
+        <button onClick={getEstates}>get estates</button>
+        <div></div>
       </div>
     </div>
   );
