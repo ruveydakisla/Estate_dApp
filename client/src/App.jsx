743bccae30cae98e3aa5c0d14799ab9a4bd77966
ruveydakisla <ruveydakisla34@gmail.com>
2023-12-20 15:54:01 +0300

    signIn page add to navigation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Index from './components/Pages/Index';
 import SignUp from './components/Pages/SignUp';
 import Navbar from '../src/components/Pages/Navbar';
 import PropertyAdd from './components/Pages/PropertyAdd';
+import SignIn from './components/Pages/SignIn';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/" element={<Index />} />
           <Route path="/SignUp" element={<SignUp />} />
           <Route path="/PropertyAdd" element={<PropertyAdd />} />
+          <Route path="/signIn" element={<SignIn />} />
           {/* Add more routes as needed */}
           {/* For example: <Route path="/about" element={<About />} /> */}
         </Routes>
